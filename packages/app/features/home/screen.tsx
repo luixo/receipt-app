d04d38f6ae572688fa7d9951c167401d83d283b9
Alexey Immoreev <lexx92@mail.ru>
2022-06-12 15:23:56 +0400

    Feature: simpler home page

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ReactNative from "react-native";
-import { styled, H1, P, A, TextLink } from "../../utils/styles";
-import { useSx } from "dripsy";
+import { styled, H1, TextLink } from "../../utils/styles";
 
 const Wrapper = styled(ReactNative.View)({
 	flex: 1,
@@ -15,44 +14,14 @@ const Header = styled(H1)({
 	textAlign: "center",
 });
 
-const Container = styled(ReactNative.View)({ maxWidth: "$container" });
-
 const Spacer = styled(ReactNative.View)({ marginTop: "$l" });
 
-const Paragraph = styled(P)({ textAlign: "center" });
-
-const Link = styled(A)({ color: "$primary" });
-
 export const HomeScreen: React.FC = () => {
-	const sx = useSx();
 	return (
 		<Wrapper>
-			<Header>Welcome to Receipt App.</Header>
-			<Container>
-				<Paragraph>At the moment this is a blank app starter.</Paragraph>
-				<Paragraph>
-					App is based on{" "}
-					<Link
-						href="https://solito.dev"
-						// @ts-expect-error react-native-web only types
-						hrefAttrs={{
-							target: "_blank",
-						}}
-					>
-						Solito
-					</Link>
-					.
-				</Paragraph>
-			</Container>
+			<Header>Welcome to Receipt App</Header>
 			<Spacer />
-			<TextLink
-				href="/receipts/"
-				textProps={{
-					style: sx({ color: "$primary" }),
-				}}
-			>
-				Link to receipts page
-			</TextLink>
+			<TextLink href="/receipts">Receipts</TextLink>
 		</Wrapper>
 	);
 };
