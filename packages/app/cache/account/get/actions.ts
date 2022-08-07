acc2cac0db0a5bdc2838b5a2e8527b75551e9744
Alexey Immoreev <lexx92@mail.ru>
2022-08-07 23:49:28 +0400

    Fix: complete invalidate on logout

--- packages/app/cache/account/get/actions.ts
+++ packages/app/cache/account/get/actions.ts
@@ -1,13 +1,10 @@
-import { TRPCReactContext, InvalidateArgs } from "app/trpc";
+import { TRPCReactContext } from "app/trpc";
 
 import { createController } from "./controller";
 import { Account } from "./types";
 
 export const set = (trpc: TRPCReactContext, account: Account) =>
 	createController(trpc).set(account);
-
-export const invalidate = (trpc: TRPCReactContext, ...args: InvalidateArgs) =>
-	createController(trpc).invalidate(...args);
 
 export const update = (
 	trpc: TRPCReactContext,
