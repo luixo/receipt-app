91a1987ae238bb9a9ab01057301f9a01efc5facb
Alexey Immoreev <lexx92@mail.ru>
2022-08-14 23:42:21 +0400

    Feature: back button

--- packages/app/components/header.tsx
+++ packages/app/components/header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { Spacer, Text, styled } from "@nextui-org/react";
+import { IoMdArrowRoundBack as BackArrow } from "react-icons/io";
+import { useRouter } from "solito/router";
 
 const Wrapper = styled("div", {
 	display: "flex",
@@ -14,6 +16,7 @@ const Title = styled(Text, {
 
 const Icon = styled("span", {
 	mr: "$4",
+	lineHeight: 0,
 });
 
 const Aside = styled("div", {
@@ -24,6 +27,7 @@ const Aside = styled("div", {
 });
 
 type Props = {
+	backHref?: string;
 	icon?: React.ReactNode;
 	children: React.ReactNode;
 	aside?: JSX.Element | JSX.Element[];
@@ -37,9 +41,14 @@ export const Header: React.FC<Props> = ({
 }) => {
 	const asideElements: JSX.Element[] | undefined =
 		aside === undefined ? aside : Array.isArray(aside) ? aside : [aside];
+	const router = useRouter();
+	const back = React.useCallback(() => router.back(), [router]);
 	return (
 		<Wrapper>
 			<Title h2 {...props}>
+				<Icon onClick={back}>
+					<BackArrow />
+				</Icon>
 				{icon ? <Icon>{icon}</Icon> : null}
 				{children}
 			</Title>
