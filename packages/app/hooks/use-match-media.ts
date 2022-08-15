4d0e8fc519613f3e43d8b53162c7b445e165d8f6
Alexey Immoreev <lexx92@mail.ru>
2022-08-15 14:45:00 +0400

    Fix: use-match-media on SSR

--- packages/app/hooks/use-match-media.ts
+++ packages/app/hooks/use-match-media.ts
@@ -9,17 +9,20 @@ const mediaEntries = Object.entries(media) as [MediaKey, string][];
 const mediaQueryLists = mediaEntries.map(([, mediaQuery]) =>
 	typeof window === "undefined" ? null : window.matchMedia(mediaQuery)
 );
-const getValue = (): MatchMediaObject =>
+const getValue = (isBrowser: boolean) => (): MatchMediaObject =>
 	mediaEntries.reduce((acc, [key], index) => {
-		acc[key] = mediaQueryLists[index]?.matches ?? false;
+		acc[key] = isBrowser ? mediaQueryLists[index]?.matches ?? false : false;
 		return acc;
 	}, {} as MatchMediaObject);
 
 export const useMatchMedia = (): MatchMediaObject => {
-	const [value, setValue] = React.useState(getValue);
+	const [value, setValue] = React.useState(
+		getValue(typeof window !== "undefined")
+	);
 	React.useLayoutEffect(() => {
-		const handler = () => setValue(getValue);
+		const handler = () => setValue(getValue(true));
 		mediaQueryLists.forEach((mql) => mql!.addEventListener("change", handler));
+		handler();
 		return () =>
 			mediaQueryLists.forEach((mql) =>
 				mql!.removeEventListener("change", handler)
