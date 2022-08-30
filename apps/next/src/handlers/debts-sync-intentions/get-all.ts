0654d608f656e4d75b6ae97cbcd20a343525d282
Alexey Immoreev <lexx92@mail.ru>
2022-08-31 02:22:41 +0400

    Refactor: factor out inbound & outbound intentions request

--- apps/next/src/handlers/debts-sync-intentions/get-all.ts
+++ apps/next/src/handlers/debts-sync-intentions/get-all.ts
@@ -1,9 +1,9 @@
 import * as trpc from "@trpc/server";
-import { sql } from "kysely";
+import { QueryCreator, sql } from "kysely";
 
 import { Currency } from "app/utils/currency";
-import { getDatabase } from "next-app/db";
-import { DebtsId, UsersId } from "next-app/db/models";
+import { getDatabase, ReceiptsDatabase } from "next-app/db";
+import { AccountsId, DebtsId, UsersId } from "next-app/db/models";
 import { AuthorizedContext } from "next-app/handlers/context";
 
 type CommonIntention = {
@@ -25,81 +25,89 @@ type InboundIntention = CommonIntention & {
 
 type OutboundIntention = CommonIntention;
 
+const getInboundIntentions = (
+	qc: QueryCreator<ReceiptsDatabase>,
+	accountId: AccountsId
+) =>
+	qc
+		.selectFrom("debtsSyncIntentions")
+		.where("debtsSyncIntentions.ownerAccountId", "<>", accountId)
+		.innerJoin("debts", (qb) =>
+			qb
+				.onRef("debts.id", "=", "debtsSyncIntentions.debtId")
+				.onRef(
+					"debts.ownerAccountId",
+					"=",
+					"debtsSyncIntentions.ownerAccountId"
+				)
+		)
+		.innerJoin("accounts", "accounts.id", "debts.ownerAccountId")
+		.innerJoin("users", "accounts.id", "users.connectedAccountId")
+		.leftJoin("debts as selfDebts", (qb) =>
+			qb
+				.onRef("selfDebts.id", "=", "debtsSyncIntentions.debtId")
+				.onRef(
+					"selfDebts.ownerAccountId",
+					"<>",
+					"debtsSyncIntentions.ownerAccountId"
+				)
+		)
+		.where("users.ownerAccountId", "=", accountId)
+		.select([
+			"debts.id",
+			"debts.ownerAccountId",
+			"debts.timestamp",
+			"debtsSyncIntentions.lockedTimestamp as intentionTimestamp",
+			"debts.amount",
+			"debts.currency",
+			"users.id as userId",
+			"debts.note",
+			"selfDebts.amount as selfAmount",
+			"selfDebts.timestamp as selfTimestamp",
+		]);
+
+const getOutboundIntentions = (
+	qc: QueryCreator<ReceiptsDatabase>,
+	accountId: AccountsId
+) =>
+	qc
+		.selectFrom("debtsSyncIntentions")
+		.where("debtsSyncIntentions.ownerAccountId", "=", accountId)
+		.innerJoin("debts", (qb) =>
+			qb
+				.onRef("debts.id", "=", "debtsSyncIntentions.debtId")
+				.onRef(
+					"debts.ownerAccountId",
+					"=",
+					"debtsSyncIntentions.ownerAccountId"
+				)
+		)
+		.select([
+			"debts.id",
+			"debts.ownerAccountId",
+			"debts.timestamp",
+			"debtsSyncIntentions.lockedTimestamp as intentionTimestamp",
+			"debts.amount",
+			"debts.currency",
+			"debts.userId",
+			"debts.note",
+			sql`null`.castTo<string | null>().as("selfAmount"),
+			sql`null`.castTo<Date | null>().as("selfTimestamp"),
+		]);
+
 export const router = trpc.router<AuthorizedContext>().query("get-all", {
 	resolve: async ({ ctx }) => {
 		const database = getDatabase(ctx);
 		const intentions = await database
 			.with("mergedIntentions", (qc) =>
-				qc
-					.selectFrom("debtsSyncIntentions")
-					.where("debtsSyncIntentions.ownerAccountId", "<>", ctx.auth.accountId)
-					.innerJoin("debts", (qb) =>
-						qb
-							.onRef("debts.id", "=", "debtsSyncIntentions.debtId")
-							.onRef(
-								"debts.ownerAccountId",
-								"=",
-								"debtsSyncIntentions.ownerAccountId"
-							)
-					)
-					.innerJoin("accounts", "accounts.id", "debts.ownerAccountId")
-					.innerJoin("users", "accounts.id", "users.connectedAccountId")
-					.leftJoin("debts as selfDebts", (qb) =>
-						qb
-							.onRef("selfDebts.id", "=", "debtsSyncIntentions.debtId")
-							.onRef(
-								"selfDebts.ownerAccountId",
-								"<>",
-								"debtsSyncIntentions.ownerAccountId"
-							)
-					)
-					.where("users.ownerAccountId", "=", ctx.auth.accountId)
-					.select([
-						"debts.id",
-						"debts.ownerAccountId",
-						"debts.timestamp",
-						"debtsSyncIntentions.lockedTimestamp as intentionTimestamp",
-						"debts.amount",
-						"debts.currency",
-						"users.id as userId",
-						"debts.note",
-						"selfDebts.amount as selfAmount",
-						"selfDebts.timestamp as selfTimestamp",
-					])
-					.union(
-						qc
-							.selectFrom("debtsSyncIntentions")
-							.where(
-								"debtsSyncIntentions.ownerAccountId",
-								"=",
-								ctx.auth.accountId
-							)
-							.innerJoin("debts", (qb) =>
-								qb
-									.onRef("debts.id", "=", "debtsSyncIntentions.debtId")
-									.onRef(
-										"debts.ownerAccountId",
-										"=",
-										"debtsSyncIntentions.ownerAccountId"
-									)
-							)
-							.select([
-								"debts.id",
-								"debts.ownerAccountId",
-								"debts.timestamp",
-								"debtsSyncIntentions.lockedTimestamp as intentionTimestamp",
-								"debts.amount",
-								"debts.currency",
-								"debts.userId",
-								"debts.note",
-								sql`null`.castTo<string | null>().as("selfAmount"),
-								sql`null`.castTo<Date | null>().as("selfTimestamp"),
-							])
-					)
+				getInboundIntentions(qc, ctx.auth.accountId).union(
+					getOutboundIntentions(qc, ctx.auth.accountId)
+				)
 			)
 			.selectFrom("mergedIntentions")
 			.selectAll()
 			.orderBy("intentionTimestamp", "desc")
+			.orderBy("id", "desc")
 			.execute();
 		return intentions.reduce<{
 			inbound: InboundIntention[];
