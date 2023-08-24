6f11579c9585dfe6838e29df6b7312f4e89932b8
Alexey Immoreev <lexx92@mail.ru>
2023-08-24 21:33:59 +0300

    Feature: set response headers in mocks

--- apps/next/tests/utils/context.ts
+++ apps/next/tests/utils/context.ts
@@ -2,23 +2,35 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { IncomingHttpHeaders } from "node:http";
 
 import type { SessionsSessionId } from "next-app/db/models";
+import type { UnauthorizedContext } from "next-app/handlers/context";
 import { createContext as createContextRaw } from "next-app/handlers/context";
 
 type ContextOptions = {
 	headers?: IncomingHttpHeaders;
 };
 
-export const createContext = (options: ContextOptions = {}) =>
+type HeaderValue = number | string | ReadonlyArray<string>;
+export type HeaderPair = { name: string; value: HeaderValue };
+type ControllerExtension = { setHeaders: HeaderPair[] };
+
+export const createContext = (
+	options: ContextOptions = {},
+): UnauthorizedContext & ControllerExtension => {
+	const setHeaders: HeaderPair[] = [];
 	// A poor emulation of real req / res, add props as needed
-	createContextRaw({
+	const context = createContextRaw({
 		req: {
 			headers: options.headers || {},
 		} as unknown as NextApiRequest,
 		res: {
 			getHeader: () => "",
-			setHeader: () => "",
+			setHeader: (name: string, value: HeaderValue) => {
+				setHeaders.push({ name, value });
+			},
 		} as unknown as NextApiResponse,
 	});
+	return { ...context, setHeaders };
+};
 
 export const createAuthContext = (
 	sessionId: SessionsSessionId = "mock-session-id",
