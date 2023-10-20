458ae3a818e4389399b60face85e87a53ae08d3d
Alexey Immoreev <lexx92@mail.ru>
2023-10-21 03:43:38 +0400

    Feature: users tests (#16)

--- apps/next/src/handlers/users/get.ts
+++ apps/next/src/handlers/users/get.ts
@@ -28,97 +28,93 @@ export const procedure = authProcedure
 		if (!maybeUser) {
 			throw new trpc.TRPCError({
 				code: "NOT_FOUND",
-				message: `User ${input.id} does not exist.`,
+				message: `No user found by id "${input.id}".`,
 			});
 		}
-		const { ownerAccountId, ...user } = maybeUser;
-		if (ownerAccountId !== ctx.auth.accountId) {
-			const errorMessage = `User ${input.id} is not owned by ${ctx.auth.accountId}`;
-			// We allow account fetch foreign users
-			// In case they share the same receipt
-			const ids = await database
-				.selectFrom("users as usersThem")
-				.where("usersThem.id", "=", input.id)
-				.innerJoin("receiptParticipants as receiptParticipantsThem", (qb) =>
-					qb.onRef("receiptParticipantsThem.userId", "=", "usersThem.id"),
-				)
-				.innerJoin("receipts", (qb) =>
-					qb.onRef("receiptParticipantsThem.receiptId", "=", "receipts.id"),
-				)
-				.innerJoin("receiptParticipants as receiptParticipantsMe", (qb) =>
-					qb.onRef("receiptParticipantsMe.receiptId", "=", "receipts.id"),
-				)
-				.innerJoin("users as usersMe", (qb) =>
-					qb
-						.onRef("receiptParticipantsMe.userId", "=", "usersMe.id")
-						.on("usersMe.connectedAccountId", "=", ctx.auth.accountId),
-				)
-				.leftJoin("accounts", (qb) =>
-					qb.onRef("usersThem.connectedAccountId", "=", "accounts.id"),
-				)
-				.leftJoin("users as usersMine", (qb) =>
-					qb.onRef("usersMine.connectedAccountId", "=", "accounts.id"),
-				)
-				.select(["usersMine.id as mineId", "usersMe.id as meId"])
-				.groupBy(["usersMine.id", "usersMe.id"])
-				.executeTakeFirst();
-			if (ids) {
-				if (ids.mineId) {
-					const myUser = await database
-						.selectFrom("users")
-						.leftJoin("accounts", (qb) =>
-							qb.onRef("connectedAccountId", "=", "accounts.id"),
-						)
-						.where("users.id", "=", ids.mineId)
-						.select([
-							"users.id as remoteId",
-							"name",
-							"publicName",
-							"accounts.email",
-							"accounts.id as accountId",
-						])
-						.limit(1)
-						.executeTakeFirstOrThrow();
-					return {
-						...myUser,
-						publicName:
-							myUser.publicName === null ? undefined : myUser.publicName,
-						email: myUser.email === null ? undefined : myUser.email,
-						localId: user.remoteId as UsersId | null,
-					};
-				}
-				const theirUser = await database
-					.selectFrom("users")
-					.leftJoin("accounts", (qb) =>
-						qb.onRef("connectedAccountId", "=", "accounts.id"),
-					)
-					.where("users.id", "=", input.id)
-					.select([
-						"users.id as remoteId",
-						"users.publicName",
-						"users.name",
-						"accounts.email",
-						"accounts.id as accountId",
-					])
-					.executeTakeFirstOrThrow();
-				return {
-					...theirUser,
-					name: theirUser.publicName || theirUser.name,
-					publicName:
-						theirUser.publicName === null ? undefined : theirUser.publicName,
-					email: theirUser.email === null ? undefined : theirUser.email,
-					localId: null as UsersId | null,
-				};
-			}
+		const { ownerAccountId, publicName, email, ...user } = maybeUser;
+		if (ownerAccountId === ctx.auth.accountId) {
+			return {
+				...user,
+				publicName: publicName === null ? undefined : publicName,
+				email: email === null ? undefined : email,
+				localId: user.remoteId as UsersId | null,
+			};
+		}
+		// We allow account fetch foreign users
+		// In case they share the same receipt
+		const userResult = await database
+			.selectFrom("users as usersThem")
+			.where("usersThem.id", "=", input.id)
+			.innerJoin("receiptParticipants as receiptParticipantsThem", (qb) =>
+				qb.onRef("receiptParticipantsThem.userId", "=", "usersThem.id"),
+			)
+			.innerJoin("receiptParticipants as receiptParticipantsMe", (qb) =>
+				qb.onRef(
+					"receiptParticipantsMe.receiptId",
+					"=",
+					"receiptParticipantsThem.receiptId",
+				),
+			)
+			// Only allow for receipts in which the fetching account is included
+			.innerJoin("users as usersMe", (qb) =>
+				qb
+					.onRef("receiptParticipantsMe.userId", "=", "usersMe.id")
+					.onRef("usersMe.ownerAccountId", "=", "usersThem.ownerAccountId")
+					.on("usersMe.connectedAccountId", "=", ctx.auth.accountId),
+			)
+			.leftJoin("accounts", (qb) =>
+				qb.onRef("usersThem.connectedAccountId", "=", "accounts.id"),
+			)
+			.leftJoin("users as usersMine", (qb) =>
+				qb
+					.onRef("usersMine.connectedAccountId", "=", "accounts.id")
+					.onRef("usersMine.ownerAccountId", "=", "usersMe.connectedAccountId"),
+			)
+			.select([
+				"usersMine.id as mineId",
+				"usersMine.name",
+				"usersMine.publicName",
+				"accounts.email",
+				"accounts.id as accountId",
+				"usersThem.name as theirName",
+				"usersThem.publicName as theirPublicName",
+			])
+			.groupBy([
+				"usersMine.id",
+				"usersMine.name",
+				"usersMine.publicName",
+				"accounts.email",
+				"accounts.id",
+				"usersThem.name",
+				"usersThem.publicName",
+			])
+			.executeTakeFirst();
+		if (!userResult) {
 			throw new trpc.TRPCError({
 				code: "FORBIDDEN",
-				message: errorMessage,
+				message: `User "${input.id}" is not owned by "${ctx.auth.email}".`,
 			});
 		}
+		if (userResult.mineId) {
+			return {
+				accountId: userResult.accountId!,
+				email: userResult.email!,
+				name: userResult.name!,
+				publicName:
+					userResult.publicName === null ? undefined : userResult.publicName,
+				remoteId: input.id,
+				localId: userResult.mineId,
+			};
+		}
 		return {
-			...user,
-			publicName: user.publicName === null ? undefined : user.publicName,
-			email: user.email === null ? undefined : user.email,
-			localId: user.remoteId as UsersId | null,
+			accountId: userResult.accountId,
+			email: userResult.email === null ? undefined : userResult.email,
+			name: userResult.theirPublicName || userResult.theirName,
+			publicName:
+				userResult.theirPublicName === null
+					? undefined
+					: userResult.theirPublicName,
+			remoteId: input.id,
+			localId: null as UsersId | null,
 		};
 	});
