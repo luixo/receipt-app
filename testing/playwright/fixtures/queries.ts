a1a5abd1608b6c95badab450fe28aad129849b38
Alexey Immoreev <lexx92@mail.ru>
2023-11-01 23:40:36 +0300

    Refactor: quieries cache mapping refactor

--- testing/playwright/fixtures/queries.ts
+++ testing/playwright/fixtures/queries.ts
@@ -56,83 +56,112 @@ const getSnapshotName = (
 	return [...path, name];
 };
 
-const getQueryCache = (page: Page) =>
-	page.evaluate(() => {
-		if (!window.getDehydratedCache) {
-			return { mutations: [], queries: [] };
-		}
-		const cache = window.getDehydratedCache();
-		const flattenError = (error: unknown) => {
-			if (
-				(error instanceof Error && error.name === "TRPCClientError") ||
-				(typeof error === "object" && error && "shape" in error)
-			) {
-				const trpcError = error as TRPCClientErrorLike<AppRouter>;
-				return {
-					message: trpcError.message,
-					data: trpcError.data
-						? {
-								code: trpcError.data.code,
-								httpStatus: trpcError.data.httpStatus,
-								path: trpcError.data.path,
-								stack: "<redacted>",
-						  }
-						: undefined,
-				};
+const getQueryCache = (page: Page, ignoreKeys: TRPCKey[]) =>
+	page.evaluate(
+		([ignoreKeysInner]) => {
+			if (!window.getDehydratedCache) {
+				return { mutations: [], queries: [] };
 			}
-			return error;
-		};
-		return {
-			queries: cache.queries.map((query) => ({
-				...query,
-				state: {
-					...query.state,
-					error: flattenError(query.state.error),
-					fetchFailureReason: undefined,
-				},
-			})),
-			mutations: cache.mutations.map((mutation) => ({
-				...mutation,
-				state: {
-					...mutation.state,
-					error: flattenError(mutation.state.error),
-					failureReason: undefined,
-				},
-			})),
-		};
-	});
-
-type QueryCache = Awaited<ReturnType<typeof getQueryCache>>;
-
-const filterCache = (cache: QueryCache, ignoreKeys?: TRPCKey[]) => {
-	if (!ignoreKeys) {
-		return cache;
-	}
-	return {
-		queries: cache.queries
-			.filter(
-				(query) =>
-					!ignoreKeys.includes(
-						(query.queryKey as QueryKey)[0].join(".") as TRPCKey,
-					),
-			)
-			.map(({ queryHash, ...query }) => ({
-				...query,
-				state: {
-					...query.state,
-					// Removing actual dates as they are not stable for snapshots
-					dataUpdatedAt: query.state.dataUpdatedAt ? -1 : 0,
-					errorUpdatedAt: query.state.errorUpdatedAt ? -1 : 0,
-				},
-			})),
-		mutations: cache.mutations.filter(
-			(mutation) =>
-				!ignoreKeys.includes(
-					(mutation.mutationKey as [string[]])[0].join(".") as TRPCKey,
+			const cache = window.getDehydratedCache();
+			const flattenError = (error: unknown) => {
+				if (
+					(error instanceof Error && error.name === "TRPCClientError") ||
+					(typeof error === "object" && error && "shape" in error)
+				) {
+					const trpcError = error as TRPCClientErrorLike<AppRouter>;
+					return {
+						message: trpcError.message,
+						data: trpcError.data
+							? {
+									code: trpcError.data.code,
+									httpStatus: trpcError.data.httpStatus,
+									path: trpcError.data.path,
+									stack: "<redacted>",
+							  }
+							: undefined,
+					};
+				}
+				return error;
+			};
+			const redactedQueries = cache.queries
+				.sort((a, b) => a.queryHash.localeCompare(b.queryHash))
+				.map(({ queryHash, queryKey, ...query }) => {
+					const typedQueryKey = queryKey as QueryKey;
+					return {
+						...query,
+						queryKey: {
+							handler: typedQueryKey[0].join(".") as TRPCKey,
+							input: JSON.stringify(typedQueryKey[1]?.input),
+						},
+						state: {
+							...query.state,
+							error: flattenError(query.state.error),
+							fetchFailureReason: undefined,
+							// Removing actual dates as they are not stable for snapshots
+							dataUpdatedAt: query.state.dataUpdatedAt ? -1 : 0,
+							errorUpdatedAt: query.state.errorUpdatedAt ? -1 : 0,
+						},
+					};
+				})
+				.filter((query) => !ignoreKeysInner!.includes(query.queryKey.handler));
+			type QueriesRecord = Record<
+				string,
+				Omit<(typeof redactedQueries)[number], "queryKey">
+			>;
+			const redactedMutations = cache.mutations
+				.map((mutation) => {
+					const mutationKey = mutation.mutationKey as QueryKey;
+					return {
+						...mutation,
+						mutationKey: {
+							handler: mutationKey[0].join(".") as TRPCKey,
+							input: JSON.stringify(mutationKey[1]?.input),
+						},
+						state: {
+							...mutation.state,
+							context: undefined,
+							error: flattenError(mutation.state.error),
+							failureReason: undefined,
+						},
+					};
+				})
+				.filter(
+					(mutation) =>
+						!ignoreKeysInner!.includes(mutation.mutationKey.handler),
+				);
+			type MutationsRecord = Record<
+				string,
+				Omit<(typeof redactedMutations)[number], "mutationKey">
+			>;
+			const lastMutationIds: Partial<Record<TRPCKey, number>> = {};
+			return {
+				queries: redactedQueries.reduce<QueriesRecord>(
+					(acc, { queryKey, ...query }) => ({
+						...acc,
+						[queryKey.input
+							? `${queryKey.handler}:[${queryKey.input}]`
+							: queryKey.handler]: query,
+					}),
+					{},
 				),
-		),
-	};
-};
+				mutations: redactedMutations.reduce<MutationsRecord>(
+					(acc, { mutationKey, ...mutation }) => {
+						lastMutationIds[mutationKey.handler] ??= 0;
+						lastMutationIds[mutationKey.handler]! += 1;
+						const lastMutationId = lastMutationIds[mutationKey.handler]!;
+						return {
+							...acc,
+							[lastMutationId === 1
+								? mutationKey.handler
+								: `${mutationKey.handler}[${lastMutationId - 1}]`]: mutation,
+						};
+					},
+					{},
+				),
+			};
+		},
+		[ignoreKeys],
+	);
 
 const remapActions = (actions: Action[]) =>
 	Object.fromEntries(
@@ -186,13 +215,10 @@ export const expectQueriesSnapshot = async <T>(
 	}: Partial<SnapshotQueryCacheOptions> = {},
 ): Promise<T> => {
 	api.clearActions();
-	const prevQueryCache = await getQueryCache(page);
+	const prevQueryCache = await getQueryCache(page, ignoreKeys);
 	const result = await fn();
-	const nextQueryCache = await getQueryCache(page);
-	const diff = objectDiff(
-		filterCache(prevQueryCache, ignoreKeys),
-		filterCache(nextQueryCache, ignoreKeys),
-	);
+	const nextQueryCache = await getQueryCache(page, ignoreKeys);
+	const diff = objectDiff(prevQueryCache, nextQueryCache);
 	withNoPlatformPath(testInfo, () => {
 		expect
 			.soft(`${JSON.stringify(diff, null, "\t")}\n`)
