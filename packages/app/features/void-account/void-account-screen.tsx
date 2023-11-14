0fcdabba694a45fd1f7d39bc73f8d260a8eccda2
Alexey Immoreev <lexx92@mail.ru>
2023-11-14 03:02:57 +0100

    Feature: void-account page tests

--- packages/app/features/void-account/void-account-screen.tsx
+++ packages/app/features/void-account/void-account-screen.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-import { Spacer } from "@nextui-org/react";
+import { Spacer, Text } from "@nextui-org/react";
 import { createParam } from "solito";
 
 import { Header } from "app/components/header";
-import { useTrpcMutationOptions } from "app/hooks/use-trpc-mutation-options";
-import { mutations } from "app/mutations";
-import { trpc } from "app/trpc";
 import type { AppPage } from "next-app/types/page";
 
 import { VoidAccount } from "./void-account";
@@ -15,15 +12,19 @@ const { useParam } = createParam<{ token: string }>();
 
 export const VoidAccountScreen: AppPage = () => {
 	const [token] = useParam("token");
-	const voidAccountMutation = trpc.auth.voidAccount.useMutation(
-		useTrpcMutationOptions(mutations.auth.voidAccount.options),
-	);
 
 	return (
 		<>
 			<Header>Void account</Header>
 			<Spacer y={1} />
-			<VoidAccount token={token} voidMutation={voidAccountMutation} />
+			{token ? (
+				<VoidAccount token={token} />
+			) : (
+				<>
+					<Text h3>Something went wrong</Text>
+					<Text b>Please verify you got void account link right</Text>
+				</>
+			)}
 		</>
 	);
 };
--- packages/app/features/void-account/void-account.tsx
+++ packages/app/features/void-account/void-account.tsx
@@ -4,7 +4,9 @@ import { Button, Loading, Spacer, Text, styled } from "@nextui-org/react";
 
 import { ErrorMessage } from "app/components/error-message";
 import { useRouter } from "app/hooks/use-router";
-import type { TRPCMutationResult } from "app/trpc";
+import { useTrpcMutationOptions } from "app/hooks/use-trpc-mutation-options";
+import { mutations } from "app/mutations";
+import { trpc } from "app/trpc";
 
 const Buttons = styled("div", {
 	display: "flex",
@@ -12,75 +14,65 @@ const Buttons = styled("div", {
 });
 
 type Props = {
-	token?: string;
-	voidMutation: TRPCMutationResult<"auth.voidAccount">;
+	token: string;
 };
 
-export const VoidAccount: React.FC<Props> = ({ token, voidMutation }) => {
+export const VoidAccount: React.FC<Props> = ({ token }) => {
 	const router = useRouter();
 	const navigateToHomePage = React.useCallback(
 		() => router.replace("/"),
 		[router],
 	);
-	const voidAccount = React.useCallback(() => {
-		if (!token) {
-			return;
-		}
-		return voidMutation.mutate({ token });
-	}, [voidMutation, token]);
-	if (!token) {
+	const voidMutation = trpc.auth.voidAccount.useMutation(
+		useTrpcMutationOptions(mutations.auth.voidAccount.options),
+	);
+	const voidAccount = React.useCallback(
+		() => voidMutation.mutate({ token }),
+		[voidMutation, token],
+	);
+	if (voidMutation.status === "success") {
 		return (
 			<>
-				<Text h3>Something went wrong</Text>
-				<Text b>Please verify you got void account link right</Text>
-			</>
-		);
-	}
-	if (voidMutation.status === "loading") {
-		return <Loading />;
-	}
-	if (voidMutation.status === "error") {
-		return <ErrorMessage message={voidMutation.error.message} />;
-	}
-	if (voidMutation.status === "idle") {
-		return (
-			<>
-				<Text h3>Are you sure you want to void your account?</Text>
+				<Text h3>{voidMutation.data.email}</Text>
+				<Spacer y={0.5} />
+				<Text h4 color="success">
+					Account removed succesfully
+				</Text>
 				<Spacer y={1} />
-				<Buttons>
-					<Button
-						onClick={voidAccount}
-						disabled={voidMutation.isLoading}
-						color="error"
-					>
-						{voidMutation.isLoading ? (
-							<Loading color="currentColor" size="sm" />
-						) : (
-							"Yes"
-						)}
-					</Button>
-					<Spacer x={0.5} />
-					<Button
-						onClick={navigateToHomePage}
-						disabled={voidMutation.isLoading}
-					>
-						No
-					</Button>
-				</Buttons>
+				<Button auto onClick={navigateToHomePage}>
+					To home page
+				</Button>
 			</>
 		);
 	}
 	return (
 		<>
-			<Text h3>{voidMutation.data.email}</Text>
-			<Spacer y={0.5} />
-			<Text h4 color="success">
-				Account removed succesfully
-			</Text>
+			<Text h3>Are you sure you want to void your account?</Text>
 			<Spacer y={1} />
-			<Button auto onClick={navigateToHomePage}>
-				To home page
-			</Button>
+			<Buttons>
+				<Button
+					onClick={voidAccount}
+					disabled={voidMutation.isLoading}
+					color="error"
+					type="submit"
+				>
+					{voidMutation.isLoading ? (
+						<Loading color="currentColor" size="sm" />
+					) : (
+						"Yes"
+					)}
+				</Button>
+				<Spacer x={0.5} />
+				<Button onClick={navigateToHomePage} disabled={voidMutation.isLoading}>
+					No
+				</Button>
+			</Buttons>
+			{voidMutation.status === "error" ? (
+				<>
+					<Spacer y={1} />
+					<ErrorMessage message={voidMutation.error.message} />
+				</>
+			) : null}
 		</>
 	);
 };
