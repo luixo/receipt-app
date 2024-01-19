848e3a6c86dab1a3762b7b2c630e061ccf491738
Alexey Immoreev <lexx92@mail.ru>
2024-01-19 23:41:17 +0400

    Fix: add index postfixes in app router

--- apps/next/src/handlers/index.ts
+++ apps/next/src/handlers/index.ts
@@ -3,14 +3,14 @@ import { t } from "next-app/handlers/trpc";
 import { router as accountRouter } from "./account/index";
 import { router as accountConnectionIntentionsRouter } from "./account-connection-intentions/index";
 import { router as accountSettingsRouter } from "./account-settings/index";
-import { router as authRouter } from "./auth";
+import { router as authRouter } from "./auth/index";
 import { router as currencyRouter } from "./currency/index";
 import { router as debtsRouter } from "./debts/index";
 import { router as itemParticipantsRouter } from "./item-participants/index";
 import { router as receiptItemsRouter } from "./receipt-items/index";
 import { router as receiptParticipantsRouter } from "./receipt-participants/index";
 import { router as receiptsRouter } from "./receipts/index";
-import { router as resetPasswordIntentionsRouter } from "./reset-password-intentions";
+import { router as resetPasswordIntentionsRouter } from "./reset-password-intentions/index";
 import { router as sessionsRouter } from "./sessions/index";
 import { router as usersRouter } from "./users/index";
 
