d95b928458425f04978a3083137e53bd340576c4
Alexey Immoreev <lexx92@mail.ru>
2024-08-15 00:21:13 +0300

    Chore: remove barrel export from web app

--- apps/web/src/utils/cookies.ts
+++ apps/web/src/utils/cookies.ts
@@ -1,15 +1,16 @@
-import * as cookie from "cookie";
+import type { CookieSerializeOptions } from "cookie";
+import { parse, serialize } from "cookie";
 import type { IncomingMessage, ServerResponse } from "http";
 
 const getCookies = (req: IncomingMessage): Partial<Record<string, string>> =>
-	cookie.parse(req.headers.cookie ? String(req.headers.cookie) : "");
+	parse(req.headers.cookie ? String(req.headers.cookie) : "");
 
 export const getCookie = (
 	req: IncomingMessage,
 	cookieName: string,
 ): string | undefined => getCookies(req)[cookieName];
 
-const DEFAULT_SET_COOKIE_OPTIONS: cookie.CookieSerializeOptions = {
+const DEFAULT_SET_COOKIE_OPTIONS: CookieSerializeOptions = {
 	httpOnly: true,
 	path: "/",
 	sameSite: "strict",
@@ -19,17 +20,15 @@ export const setCookie = (
 	res: ServerResponse,
 	cookieName: string,
 	cookieValue: string,
-	opts: cookie.CookieSerializeOptions = {},
+	opts: CookieSerializeOptions = {},
 ) => {
 	const setCookieHeader = res.getHeader("set-cookie") || "";
 	res.setHeader(
 		"set-cookie",
 		setCookieHeader.toString() +
-			cookie.serialize(cookieName, cookieValue, {
+			serialize(cookieName, cookieValue, {
 				...DEFAULT_SET_COOKIE_OPTIONS,
 				...opts,
 			}),
 	);
 };
-
-export * from "cookie";
