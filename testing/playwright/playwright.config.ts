d7afdcc8c3756f879ab76bfa6f024bac07c0af37
Alexey Immoreev <lexx92@mail.ru>
2025-06-24 02:01:10 +0300

    Feature(tests): zero tolerance to flaky tests

--- testing/playwright/playwright.config.ts
+++ testing/playwright/playwright.config.ts
@@ -86,6 +86,8 @@ export default defineConfig({
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
+	// We're brave enough to believe we don't have flaky tests
+	failOnFlakyTests: true,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
