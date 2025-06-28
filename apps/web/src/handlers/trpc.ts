075db64d401a0a73a74365797daf8e3e08fc4406
Alexey Immoreev <lexx92@mail.ru>
2025-06-28 06:49:47 +0300

    Feature: batch authorization middleware

--- apps/web/src/handlers/trpc.ts
+++ apps/web/src/handlers/trpc.ts
@@ -1,4 +1,5 @@
 import { TRPCError, initTRPC } from "@trpc/server";
+import { isNonNullish, unique } from "remeda";
 
 import { AUTH_COOKIE } from "~app/utils/auth";
 import {
@@ -6,11 +7,13 @@ import {
 	pretendUserSchema,
 } from "~app/utils/store/pretend-user";
 import { transformer } from "~app/utils/trpc";
+import type { AccountsId } from "~db/models";
 import {
 	SESSION_EXPIRATION_DURATION,
 	SESSION_SHOULD_UPDATE_EVERY,
 	getExpirationDate,
 } from "~web/handlers/auth/utils";
+import { queueCallFactory } from "~web/handlers/batch";
 import type { NetContext, UnauthorizedContext } from "~web/handlers/context";
 import { formatErrorMessage } from "~web/handlers/errors";
 import { sessionIdSchema } from "~web/handlers/validation";
@@ -45,10 +48,10 @@ export const unauthProcedure = t.procedure.use(
 	},
 );
 
+const getAuthToken = (ctx: UnauthorizedContext) =>
+	getCookie(getReqHeader(ctx, "cookie"), AUTH_COOKIE);
+
 const getPretendAccountEmail = (ctx: NetContext): string | undefined => {
-	if (getReqHeader(ctx, "x-keep-real-auth")) {
-		return;
-	}
 	const pretendUserString = getCookie(
 		getReqHeader(ctx, "cookie"),
 		PRETEND_USER_STORE_NAME,
@@ -60,8 +63,139 @@ const getPretendAccountEmail = (ctx: NetContext): string | undefined => {
 	return user.email;
 };
 
+const queueSession = queueCallFactory<
+	UnauthorizedContext,
+	{ authToken: string },
+	{
+		realAuth: {
+			accountId: AccountsId;
+			email: string;
+		};
+		auth: {
+			accountId: AccountsId;
+			email: string;
+		};
+		role: string | undefined;
+	}
+>(
+	(ctx) => async (inputs) => {
+		const authTokens = unique(inputs.map(({ authToken }) => authToken));
+		const pretendAccountEmail = getPretendAccountEmail(ctx);
+		const [sessions, pretendAccount] = await Promise.all([
+			ctx.database
+				.selectFrom("sessions")
+				.innerJoin("accounts", (qb) =>
+					qb.onRef("accounts.id", "=", "sessions.accountId"),
+				)
+				.select([
+					"sessions.accountId",
+					"accounts.email",
+					"accounts.role",
+					"sessions.expirationTimestamp",
+					"sessions.sessionId",
+				])
+				.where((eb) =>
+					eb("sessions.sessionId", "in", authTokens).and(
+						"sessions.expirationTimestamp",
+						">",
+						new Date(),
+					),
+				)
+				.execute(),
+			pretendAccountEmail
+				? ctx.database
+						.selectFrom("accounts")
+						.where("accounts.email", "=", pretendAccountEmail)
+						.select(["accounts.id", "accounts.email"])
+						.limit(1)
+						.executeTakeFirst()
+				: undefined,
+		]);
+		const sessionsOrErrors = inputs.map((input) => {
+			const matchedSession = sessions.find(
+				(session) => session.sessionId === input.authToken,
+			);
+			if (!matchedSession) {
+				return new TRPCError({
+					code: "UNAUTHORIZED",
+					message: "Session id mismatch",
+				});
+			}
+			const auth = {
+				accountId: matchedSession.accountId,
+				email: matchedSession.email,
+			};
+			if (
+				pretendAccount &&
+				matchedSession.role === "admin" &&
+				!getReqHeader(ctx, "x-keep-real-auth")
+			) {
+				return {
+					realAuth: auth,
+					auth: {
+						accountId: pretendAccount.id,
+						email: pretendAccount.email,
+					},
+					role: matchedSession.role as "admin",
+				};
+			}
+			return {
+				realAuth: auth,
+				auth,
+				role: matchedSession.role ?? undefined,
+			};
+		});
+		const updateableSessions = authTokens
+			.map((authToken) => {
+				const matchedSession = sessions.find(
+					(session) => session.sessionId === authToken,
+				);
+				if (
+					!matchedSession ||
+					matchedSession.expirationTimestamp.valueOf() - Date.now() >=
+						SESSION_EXPIRATION_DURATION - SESSION_SHOULD_UPDATE_EVERY
+				) {
+					return;
+				}
+				return {
+					authToken,
+					nextExpirationTimestamp: getExpirationDate(),
+				};
+			})
+			.filter(isNonNullish);
+		void ctx.database.transaction().execute(async (tx) => {
+			await Promise.all(
+				updateableSessions.map(
+					async ({ authToken, nextExpirationTimestamp }) => {
+						await tx
+							.updateTable("sessions")
+							.set({ expirationTimestamp: nextExpirationTimestamp })
+							.where("sessionId", "=", authToken)
+							.executeTakeFirst();
+						setCookie(ctx, AUTH_COOKIE, authToken, {
+							expires: nextExpirationTimestamp,
+						});
+					},
+				),
+			);
+		});
+		return sessionsOrErrors;
+	},
+	{
+		getKey: (ctx) =>
+			[
+				// If we've got here, auth token surely exists
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				getAuthToken(ctx)!,
+				getReqHeader(ctx, "x-keep-real-auth") ?? "",
+			]
+				.filter(Boolean)
+				.join("/"),
+	},
+);
+
 export const authProcedure = unauthProcedure.use(async ({ ctx, next }) => {
-	const authToken = getCookie(getReqHeader(ctx, "cookie"), AUTH_COOKIE);
+	const authToken = getAuthToken(ctx);
 	if (typeof authToken !== "string" || !authToken) {
 		throw new TRPCError({
 			code: "UNAUTHORIZED",
@@ -75,73 +209,19 @@ export const authProcedure = unauthProcedure.use(async ({ ctx, next }) => {
 			message: "Session id mismatch",
 		});
 	}
-	const { database } = ctx;
-	const session = await database
-		.selectFrom("sessions")
-		.innerJoin("accounts", (qb) =>
-			qb.onRef("accounts.id", "=", "sessions.accountId"),
-		)
-		.select([
-			"sessions.accountId",
-			"accounts.email",
-			"accounts.role",
-			"sessions.expirationTimestamp",
-		])
-		.where((eb) =>
-			eb("sessions.sessionId", "=", authToken).and(
-				"sessions.expirationTimestamp",
-				">",
-				new Date(),
-			),
-		)
-		.limit(1)
-		.executeTakeFirst();
-	if (!session) {
-		throw new TRPCError({
-			code: "UNAUTHORIZED",
-			message: "Session id mismatch",
-		});
-	}
-	if (
-		session.expirationTimestamp.valueOf() - Date.now() <
-		SESSION_EXPIRATION_DURATION - SESSION_SHOULD_UPDATE_EVERY
-	) {
-		const nextExpirationTimestamp = getExpirationDate();
-		void database
-			.updateTable("sessions")
-			.set({ expirationTimestamp: nextExpirationTimestamp })
-			.where("sessionId", "=", authToken)
-			.executeTakeFirst();
-		setCookie(ctx, AUTH_COOKIE, authToken, {
-			expires: nextExpirationTimestamp,
-		});
-	}
-	let auth = {
-		accountId: session.accountId,
-		email: session.email,
-	};
-	const pretendAccountEmail = getPretendAccountEmail(ctx);
-	if (pretendAccountEmail && session.role === "admin") {
-		const pretendAccount = await database
-			.selectFrom("accounts")
-			.where("accounts.email", "=", pretendAccountEmail)
-			.select(["accounts.id", "accounts.email"])
-			.limit(1)
-			.executeTakeFirst();
-		if (pretendAccount) {
-			auth = {
-				accountId: pretendAccount.id,
-				email: pretendAccount.email,
-			};
-		}
-	}
+	const { realAuth, auth, role } = await queueSession({
+		ctx,
+		input: { authToken },
+		signal: new AbortController().signal,
+	});
 	return next({
 		ctx: {
 			...ctx,
-			logger: ctx.logger.child({ accountId: session.accountId }),
+			logger: ctx.logger.child({ accountId: auth.accountId }),
+			realAuth,
 			auth,
 			authToken,
-			role: session.role ?? undefined,
+			role,
 		},
 	});
 });
@@ -153,5 +233,5 @@ export const adminProcedure = authProcedure.use(async ({ ctx, next }) => {
 			message: "Admin procedure is available only if you're an admin",
 		});
 	}
-	return next();
+	return next({ ctx: { ...ctx, auth: ctx.realAuth } });
 });
