1aabd0ca4245407acb8734964a00c0dcc14e2d8e
Alexey Immoreev <lexx92@mail.ru>
2025-07-02 01:52:51 +0300

    Feature: preload account data for account page

--- apps/web/src/pages/_protected/account.tsx
+++ apps/web/src/pages/_protected/account.tsx
@@ -2,11 +2,16 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import { AccountScreen } from "~app/features/account/account-screen";
 import { getTitle, loadNamespaces } from "~app/utils/i18n";
+import { getLoaderTrpcClient } from "~web/utils/trpc";
 
 export const Route = createFileRoute("/_protected/account")({
 	component: AccountScreen,
 	loader: async (ctx) => {
 		await loadNamespaces(ctx.context, "account");
+		const trpc = getLoaderTrpcClient(ctx.context);
+		await ctx.context.queryClient.prefetchQuery(
+			trpc.account.get.queryOptions(),
+		);
 	},
 	head: ({ match }) => ({
 		meta: [{ title: getTitle(match.context, "account") }],
