2ff5a30c33c8ce2d7bd96286ee0261bce3326fa3
Alexey Immoreev <lexx92@mail.ru>
2025-07-06 07:12:24 +0300

    Fix: temporary fix for errors in prefetch

--- apps/web/src/utils/ssr.tsx
+++ apps/web/src/utils/ssr.tsx
@@ -2,11 +2,15 @@ import type React from "react";
 
 import { useQueryClient } from "@tanstack/react-query";
 import { useRouterState } from "@tanstack/react-router";
+import { TRPCClientError } from "@trpc/client";
 import type { ResolverDef, TRPCQueryOptions } from "@trpc/tanstack-react-query";
 
 import { useBooleanState } from "~app/hooks/use-boolean-state";
 import { useMountEffect } from "~app/hooks/use-mount-effect";
 import type { RouterContext } from "~web/pages/__root";
+
+// see https://github.com/TanStack/router/issues/4084
+const ERROR_TAG = "__error__";
 
 export const prefetch = (
 	context: RouterContext,
@@ -14,7 +18,10 @@ export const prefetch = (
 ) =>
 	optionsSet.map((options) => ({
 		queryKey: options.queryKey,
-		promise: context.queryClient.fetchQuery(options),
+		promise: context.queryClient.fetchQuery(options).catch((error) => ({
+			[ERROR_TAG]: true,
+			message: String(error),
+		})),
 	}));
 
 export const HydrationBoundary: React.FC<React.PropsWithChildren> = ({
@@ -35,7 +42,12 @@ export const HydrationBoundary: React.FC<React.PropsWithChildren> = ({
 		prefetchedQueries.forEach((query) => {
 			void localQueryClient.prefetchQuery({
 				queryKey: query.queryKey,
-				queryFn: () => query.promise,
+				queryFn: () =>
+					query.promise.then((result) => {
+						if (ERROR_TAG in result) {
+							throw new TRPCClientError(result.message);
+						}
+					}),
 			});
 		});
 	}
