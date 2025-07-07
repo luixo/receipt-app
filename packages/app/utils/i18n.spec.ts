ddc530e6ec88489923ae87f905e2af19ca7637fa
Alexey Immoreev <lexx92@mail.ru>
2025-07-07 18:00:33 +0300

    Fix: i18n page tests

--- packages/app/utils/i18n.spec.ts
+++ packages/app/utils/i18n.spec.ts
@@ -4,46 +4,71 @@ import type { Language } from "~app/utils/i18n-data";
 import { expect, test as originalTest } from "~tests/frontend/fixtures";
 import { i18nFixtures } from "~tests/frontend/fixtures/i18n";
 
-const test = mergeTests(originalTest, i18nFixtures);
+type Fixtures = {
+	openSettings: () => Promise<void>;
+	openAccount: () => Promise<void>;
+};
+
+const test = mergeTests(originalTest, i18nFixtures).extend<Fixtures>({
+	openSettings: ({ page, awaitCacheKey, api }, use) =>
+		use(async () => {
+			await page.goto("/settings");
+			api.mockUtils.authPage();
+			api.mockFirst("accountSettings.get", { manualAcceptDebts: true });
+			await awaitCacheKey("accountSettings.get");
+		}),
+	openAccount: ({ page, awaitCacheKey, api }, use) =>
+		use(async () => {
+			await page.goto("/accont");
+			api.mockUtils.authPage();
+			await awaitCacheKey("account.get");
+		}),
+});
 
 test.use({ locale: "en-GB" });
 test.describe("Language strategies", () => {
-	test("Cookie", async ({ page, getI18nResource, setLanguageCookie }) => {
+	test("Cookie", async ({
+		openSettings,
+		page,
+		getI18nResource,
+		setLanguageCookie,
+	}) => {
 		await setLanguageCookie("ru");
-		await page.goto("/settings");
+		await openSettings();
 		const ru = await getI18nResource("ru", "settings");
 		await expect(page.locator("h1")).toHaveText(ru.header);
 	});
 
 	test.describe("Preferred language header", () => {
 		test.use({ locale: "ru-RU" });
-		test("Full tag", async ({ page, getI18nResource }) => {
-			await page.goto("/settings");
+		test("Full tag", async ({ openSettings, page, getI18nResource }) => {
+			await openSettings();
 			const ru = await getI18nResource("ru", "settings");
 			await expect(page.locator("h1")).toHaveText(ru.header);
 		});
 
 		test.use({ locale: "ru" });
-		test("Short tag", async ({ page, getI18nResource }) => {
-			await page.goto("/settings");
+		test("Short tag", async ({ openSettings, page, getI18nResource }) => {
+			await openSettings();
 			const ru = await getI18nResource("ru", "settings");
 			await expect(page.locator("h1")).toHaveText(ru.header);
 		});
 	});
 
-	test("Fallback", async ({ page, getI18nResource }) => {
-		await page.goto("/settings");
+	test("Fallback", async ({ openSettings, page, getI18nResource }) => {
+		await openSettings();
 		const en = await getI18nResource("en", "settings");
 		await expect(page.locator("h1")).toHaveText(en.header);
 	});
 
 	test("Invalid language", async ({
+		openSettings,
 		page,
 		getI18nResource,
 		setLanguageCookie,
 	}) => {
 		await setLanguageCookie("de" as Language);
-		await page.goto("/settings");
+		await openSettings();
 		const en = await getI18nResource("en", "settings");
 		await expect(page.locator("h1")).toHaveText(en.header);
 	});
@@ -53,12 +78,13 @@ test.describe("Server-side translations", () => {
 	test.use({ javaScriptEnabled: false });
 
 	test("Static - head", async ({
+		openSettings,
 		page,
 		getI18nResource,
 		setLanguageCookie,
 	}) => {
 		await setLanguageCookie("ru");
-		await page.goto("/settings");
+		await openSettings();
 		const ru = await getI18nResource("ru", "default");
 		expect(await page.title()).toEqual(
 			ru.titles.template.replace("{{page}}", ru.titles.settings),
@@ -66,12 +92,13 @@ test.describe("Server-side translations", () => {
 	});
 
 	test("React - on a page", async ({
+		openSettings,
 		page,
 		getI18nResource,
 		setLanguageCookie,
 	}) => {
 		await setLanguageCookie("ru");
-		await page.goto("/settings");
+		await openSettings();
 		const ru = await getI18nResource("ru", "settings");
 		await expect(page.locator("h1")).toHaveText(ru.header);
 	});
@@ -79,18 +106,23 @@ test.describe("Server-side translations", () => {
 
 test.describe("Client-side translations", () => {
 	test("Initial load does not fetch extra data", async ({
+		openAccount,
 		page,
 		api,
 		getNamespaces,
 	}) => {
 		const { user } = api.mockUtils.authPage();
-		await page.goto("/account");
+		await openAccount();
 		await expect(page.locator("h1")).toHaveText(user.name);
 		expect(await getNamespaces()).toEqual(["default", "account"]);
 	});
 
-	test("Changing namespace loads data", async ({ page, getNamespaces }) => {
-		await page.goto("/account");
+	test("Changing namespace loads data", async ({
+		openAccount,
+		page,
+		getNamespaces,
+	}) => {
+		await openAccount();
 		expect(await getNamespaces()).toEqual(["default", "account"]);
 		await page.locator("a[href='/settings']").click();
 		await expect
@@ -99,11 +131,12 @@ test.describe("Client-side translations", () => {
 	});
 
 	test("Changing language loads data", async ({
+		openSettings,
 		page,
 		getI18nResource,
 		getLanguages,
 	}) => {
-		await page.goto("/settings");
+		await openSettings();
 		expect(await getLanguages()).toEqual(["en"]);
 		const en = await getI18nResource("en", "settings");
 		await expect(page.locator("h1")).toHaveText(en.header);
