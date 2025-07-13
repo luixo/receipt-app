8fecf69b28e3d33675de6bc43598d4edfaa15be8
Alexey Immoreev <lexx92@mail.ru>
2025-07-13 16:04:24 +0300

    Fix: remove unused ssr: false props

--- packages/app/hooks/use-debts-intentions.ts
+++ packages/app/hooks/use-debts-intentions.ts
@@ -4,10 +4,6 @@ import { useTRPC } from "~app/utils/trpc";
 
 export const useDebtsIntentions = () => {
 	const trpc = useTRPC();
-	const intentions = useQuery(
-		trpc.debtIntentions.getAll.queryOptions(undefined, {
-			trpc: { ssr: false },
-		}),
-	);
+	const intentions = useQuery(trpc.debtIntentions.getAll.queryOptions());
 	return intentions.status === "success" ? intentions.data.length : 0;
 };
