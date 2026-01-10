95891cc0979ca4a6d0ad3104d81d1c318f762b49
Alexey Immoreev <lexx92@mail.ru>
2026-01-10 05:31:36 +0400

    Fix: prettier plugins in VSCode

--- utils/format/config.ts
+++ utils/format/config.ts
@@ -1,14 +1,11 @@
 import type { Config } from "prettier";
-// @ts-expect-error This package doesn't have types
-import packageJsonPlugin from "prettier-plugin-packagejson";
-import * as tailwindPlugin from "prettier-plugin-tailwindcss";
 
 export const config: Config = {
 	semi: true,
 	useTabs: true,
 	tabWidth: 2,
 	singleQuote: false,
-	plugins: [tailwindPlugin, packageJsonPlugin],
+	plugins: ["prettier-plugin-packagejson", "prettier-plugin-tailwindcss"],
 	tailwindStylesheet: "./packages/app/global.css",
 	tailwindFunctions: ["tv", "cn"],
 };
