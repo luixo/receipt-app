3760ccfed08025e69de8f0dc71af17b2710f2cdb
Alexey Immoreev <lexx92@mail.ru>
2026-01-12 06:32:18 +0400

    Fix: add uniwind to native

--- apps/mobile/metro.config.ts
+++ apps/mobile/metro.config.ts
@@ -1,6 +1,22 @@
 import { getDefaultConfig } from "expo/metro-config.js";
+import { createRequire } from "node:module";
+import path from "node:path";
+
+const require = createRequire(import.meta.url);
+const { withUniwindConfig } =
+	// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+	require("uniwind/metro") as typeof import("uniwind/metro");
 
 const projectRoot = import.meta.dirname;
-const config = getDefaultConfig(projectRoot, {});
+const defaultConfig = getDefaultConfig(projectRoot, {});
+
+const config = withUniwindConfig(
+	// @ts-expect-error Types slightly diverge here
+	defaultConfig satisfies Parameters<typeof withUniwindConfig>[0],
+	{
+		cssEntryFile: "./app.css",
+		dtsFile: path.join(projectRoot, "../../packages/app/uniwind-types.d.ts"),
+	},
+);
 
 export default config;
