55b5837c965533525aa17283bc2af679663b7ec3
Alexey Immoreev <lexx92@mail.ru>
2026-02-09 03:14:32 +0400

    Refacotr: move empty specific exchange screen to a module

--- apps/web/src/pages/_protected/debts/user/$id/exchange/specific.tsx
+++ apps/web/src/pages/_protected/debts/user/$id/exchange/specific.tsx
@@ -1,13 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { DebtsExchangeSpecificScreen } from "~app/features/debts-exchange-specific/debts-exchange-specific-screen";
 import { getTitle } from "~web/utils/i18n";
-
-const Wrapper = () => null;
 
 export const Route = createFileRoute(
 	"/_protected/debts/user/$id/exchange/specific",
 )({
-	component: Wrapper,
+	component: DebtsExchangeSpecificScreen,
 	loader: async (ctx) => {
 		await ctx.context.i18nContext.loadNamespaces("debts");
 	},
