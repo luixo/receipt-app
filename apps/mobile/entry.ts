2511389bb78d54446f8f5928f45c412cde5ef108
Alexey Immoreev <lexx92@mail.ru>
2026-02-16 03:30:37 +0400

    Fix: expo router location & format shim

--- apps/mobile/entry.ts
+++ apps/mobile/entry.ts
@@ -4,4 +4,17 @@
 // But it does not
 // Expo Go version of the app runs ok nevertheless
 import "react-native/Libraries/Renderer/shims/ReactNative";
+// It is expected to have .js extensions for these polyfills
+/* eslint-disable import-x/extensions,import-x/no-unresolved */
+import "@formatjs/intl-pluralrules/polyfill-force.js";
+import "@formatjs/intl-pluralrules/locale-data/en.js";
+import "@formatjs/intl-pluralrules/locale-data/ru.js";
+import "@formatjs/intl-locale/polyfill-force.js";
+import "@formatjs/intl-displaynames/polyfill-force.js";
+import "@formatjs/intl-displaynames/locale-data/en.js";
+import "@formatjs/intl-displaynames/locale-data/ru.js";
+import "@formatjs/intl-numberformat/polyfill-force.js";
+import "@formatjs/intl-numberformat/locale-data/en.js";
+import "@formatjs/intl-numberformat/locale-data/ru.js";
+/* eslint-enable import-x/extensions,import-x/no-unresolved */
 import "expo-router/entry";
