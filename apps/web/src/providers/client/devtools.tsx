2b597a13011aa7fbb9bad8e035e4eca5c604234e
Alexey Immoreev <lexx92@mail.ru>
2026-08-11 03:51:09 +0300

    Fix: update devtools

--- apps/web/src/providers/client/devtools.tsx
+++ apps/web/src/providers/client/devtools.tsx
@@ -5,34 +5,28 @@ import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import { useBooleanState } from "~app/hooks/use-boolean-state";
-import { useHotkey } from "~web/hooks/use-hotkey";
-
 export const DevToolsProvider: React.FC<React.PropsWithChildren<object>> = ({
 	children,
-}) => {
-	const [isMounted, { switchValue: switchMounted }] = useBooleanState();
-	useHotkey(["shift"], "x", switchMounted);
-	return (
-		<>
-			{children}
-			{/* When running with '--mode test' hydration mismatch error appears */}
-			{isMounted ? (
-				<TanStackDevtools
-					// config={{ triggerHidden: import.meta.env.MODE === "test" }}
-					plugins={[
-						{
-							name: "TanStack Query",
-							render: <ReactQueryDevtoolsPanel />,
-						},
-						{
-							name: "TanStack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-						formDevtoolsPlugin(),
-					]}
-				/>
-			) : null}
-		</>
-	);
-};
+}) => (
+	<>
+		{children}
+		{/* When running with '--mode test' hydration mismatch error appears */}
+		<TanStackDevtools
+			config={{
+				triggerHidden: true,
+				openHotkey: ["CtrlOrMeta", "X"],
+			}}
+			plugins={[
+				{
+					name: "TanStack Query",
+					render: <ReactQueryDevtoolsPanel />,
+				},
+				{
+					name: "TanStack Router",
+					render: <TanStackRouterDevtoolsPanel />,
+				},
+				formDevtoolsPlugin(),
+			]}
+		/>
+	</>
+);
