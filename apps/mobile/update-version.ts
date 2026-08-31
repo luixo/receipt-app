656c83ba8c98a7f2200b1c258b37b4e33c902166
Alexey Immoreev <github@luixo.me>
2026-08-31 18:42:47 +0400

    Chore: upgrade typescript (#57)

---
+++ apps/mobile/update-version.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+// eslint-disable-next-line import-js/no-extraneous-dependencies
+import semver from "semver";
+
+const versionFilePath = path.join(import.meta.dirname, "./.version");
+
+const currentVersion = await fs.readFile(versionFilePath, "utf8");
+const nextVersion =
+	semver.inc(currentVersion.trim(), "patch") ?? currentVersion;
+await fs.writeFile(versionFilePath, nextVersion);
+// eslint-disable-next-line no-console
+console.log(
+	`Update .version "${currentVersion.trim()}" -> "${nextVersion}" successfully`,
+);
