40aa5c31dc43602550f3cc80947c4dba0a9c865a
Alexey Immoreev <lexx92@mail.ru>
2026-09-15 01:38:51 +0300

    Fix(tests): server messages

--- testing/playwright/playwright.config.ts
+++ testing/playwright/playwright.config.ts
@@ -1,4 +1,4 @@
-import type { Project, ReporterDescription } from "@playwright/test";
+import type { Project } from "@playwright/test";
 import { defineConfig, devices } from "@playwright/test";
 import path from "node:path";
 
@@ -83,13 +83,12 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI
-		? "blob"
-		: [
-				["html", { open: "never" }] satisfies ReporterDescription,
-				["list"],
-				["./server-reporter.ts"],
-			],
+	reporter: [
+		...(process.env.CI
+			? ([["blob"], ["dot"], ["github"]] as const)
+			: ([["html", { open: "never" }], ["list"]] as const)),
+		["./server-reporter.ts"],
+	],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
